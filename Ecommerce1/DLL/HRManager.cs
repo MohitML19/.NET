18db61eaa0b75b2f704326056e0da3627e633ef7
MohitML19 <131249596+MohitML19@users.noreply.github.com>
2023-07-20 03:03:37 +0530

    Add files via upload

---
+++ Ecommerce1/DLL/HRManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+using DAL;
+namespace DLL
+{
+    public class HRManager
+    {
+        public List<Department> GetDepartments()
+        {
+            List<Department> list = new List<Department>();
+            list = DBManager.allDepartments();
+            return list;
+        }
+    }
+}
