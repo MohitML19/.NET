48558174055fdd73d55a7db329d2e25df2165dcd
MohitML19 <131249596+MohitML19@users.noreply.github.com>
2023-07-20 03:00:52 +0530

    Add files via upload

---
+++ Ecommerce1/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using BOL;
+using DAL;
+using DLL;
+List<Department> list = DBManager.allDepartments();
+
+foreach (Department dept in list)
+{
+    Console.WriteLine(dept.Name + " " + dept.Location);
+}
+Console.WriteLine("----------------------------------------------------");
+
+Department d1 = DBManager.getDepartment(1);
+Console.WriteLine(d1.Name + " " + d1.Location);
+
+Console.WriteLine("-----------------------------------------------------");
+Department d2 = new Department("RnD","Satara");
+bool b = DBManager.insertDept(d2);
+Console.WriteLine(b);
